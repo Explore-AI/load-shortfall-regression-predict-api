e18a68a0e2ca64cfd9530662cd557cf08ae81595
lnjoctrey <30340090+lnjoctrey@users.noreply.github.com>
2022-03-03 14:22:47 +0200

    Update model.py

--- model.py
+++ model.py
@@ -64,7 +64,7 @@ def _preprocess_data(data):
 
     feature_vector_df['Seville_pressure'] = feature_vector_df['Seville_pressure'].str.extract('(\d+)')
     feature_vector_df['Seville_pressure'] =pd.to_numeric(feature_vector_df['Seville_pressure'], downcast = 'float')
-    feature_vector_df['Valencia_pressure'] = df['Valencia_pressure'].fillna(value=df['Valencia_pressure'].mean())
+    feature_vector_df['Valencia_pressure'] = feature_vector_df['Valencia_pressure'].fillna(value=df['Valencia_pressure'].mean())
 
     predict_vector = feature_vector_df[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
        'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
