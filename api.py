f8e6aaf05cb145c1b1c0d37d70999e1fd226f505
lnjoctrey <30340090+lnjoctrey@users.noreply.github.com>
2022-03-03 10:37:46 +0200

    Update api.py

--- api.py
+++ api.py
@@ -62,4 +62,4 @@ def model_prediction():
 # This will allow Flask to automatically restart itself everytime you
 # update your API code.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
