facfcc7e7438cbd9c9306d34a8eb9694f97dcc7a
lnjoctrey <30340090+lnjoctrey@users.noreply.github.com>
2022-03-03 13:52:03 +0200

    Update model.py

--- model.py
+++ model.py
@@ -59,6 +59,8 @@ def _preprocess_data(data):
 
     # ----------- Replace this code with your own preprocessing steps --------
     
+    feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)')
+    feature_vector_df['Valencia_wind_deg'] =pd.to_numeric(feature_vector_df['Valencia_wind_deg'], downcast = 'float')
 
     predict_vector = feature_vector_df[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
        'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
