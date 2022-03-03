d74dac84bdbfa25214bd81ea78b782c602d63214
lnjoctrey <30340090+lnjoctrey@users.noreply.github.com>
2022-03-03 14:27:33 +0200

    Update model.py

--- model.py
+++ model.py
@@ -58,7 +58,7 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    df = data
+    predict_vector = feature_vector_df
     feature_vector_df['Valencia_wind_deg'] = feature_vector_df['Valencia_wind_deg'].str.extract('(\d+)')
     feature_vector_df['Valencia_wind_deg'] =pd.to_numeric(feature_vector_df['Valencia_wind_deg'], downcast = 'float')
 
@@ -66,21 +66,7 @@ def _preprocess_data(data):
     feature_vector_df['Seville_pressure'] =pd.to_numeric(feature_vector_df['Seville_pressure'], downcast = 'float')
     feature_vector_df['Valencia_pressure'] = feature_vector_df['Valencia_pressure'].fillna(value=df['Valencia_pressure'].mean())
 
-    predict_vector = feature_vector_df[['Madrid_wind_speed', 'Valencia_wind_deg', 'Bilbao_rain_1h',
-       'Valencia_wind_speed', 'Seville_humidity', 'Madrid_humidity',
-       'Bilbao_clouds_all', 'Bilbao_wind_speed', 'Seville_clouds_all',
-       'Bilbao_wind_deg', 'Barcelona_wind_speed', 'Barcelona_wind_deg',
-       'Madrid_clouds_all', 'Seville_wind_speed', 'Barcelona_rain_1h',
-       'Seville_pressure', 'Seville_rain_1h', 'Bilbao_snow_3h',
-       'Barcelona_pressure', 'Seville_rain_3h', 'Madrid_rain_1h',
-       'Barcelona_rain_3h', 'Valencia_snow_3h', 'Madrid_weather_id',
-       'Barcelona_weather_id', 'Bilbao_pressure', 'Seville_weather_id',
-       'Valencia_pressure', 'Seville_temp_max', 'Madrid_pressure',
-       'Valencia_temp_max', 'Valencia_temp', 'Bilbao_weather_id',
-       'Seville_temp', 'Valencia_humidity', 'Valencia_temp_min',
-       'Barcelona_temp_max', 'Madrid_temp_max', 'Barcelona_temp',
-       'Bilbao_temp_min', 'Bilbao_temp', 'Barcelona_temp_min',
-       'Bilbao_temp_max', 'Seville_temp_min', 'Madrid_temp', 'Madrid_temp_min']]
+    
     # ------------------------------------------------------------------------
 
     return predict_vector
