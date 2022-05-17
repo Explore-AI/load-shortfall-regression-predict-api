c27e43c91d749f7b01022f181c8f6285348a6941
moolivieJHB <moolivie@student.wethinkcode.co.za>
2022-05-17 23:04:30 +0200

    api data processing updates ,still much work needed

--- model.py
+++ model.py
@@ -26,6 +26,10 @@ import numpy as np
 import pandas as pd
 import pickle
 import json
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import *
+
+
 
 def _preprocess_data(data):
     """Private helper function to preprocess data for model prediction.
@@ -44,6 +48,8 @@ def _preprocess_data(data):
     Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
         The preprocessed data, ready to be used our model for prediction.
     """
+    
+
     # Convert the json string to a python dictionary object
     feature_vector_dict = json.loads(data)
     # Load the dictionary as a Pandas DataFrame.
@@ -58,8 +64,34 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    # predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
     # ------------------------------------------------------------------------
+
+
+    feature_vector_df ['time'] = pd.to_datetime(feature_vector_df['time'])
+
+    dum_cols = ['Valencia_wind_deg', 'Seville_pressure']
+    df_dummies = pd.get_dummies(feature_vector_df, prefix='dummies', prefix_sep='_', columns=dum_cols, dtype=int, drop_first=True)
+
+    df_dummies.columns = [col.replace(" ","_") for col in df_dummies.columns]
+
+
+
+    dum_title = [i for i in df_dummies.columns if i != 'load_shortfall_3h'] + ['load_shortfall_3h']
+    df_dummies = df_dummies.reindex(columns=dum_title)
+
+    feature_vector_df  = df_dummies.copy()
+
+    X = feature_vector_df.drop(['load_shortfall_3h', 'time'], axis=1)
+    y = feature_vector_df['load_shortfall_3h']
+
+    scaler = StandardScaler()
+    X_scaled = scaler.fit_transform(X)
+
+    X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
+    X_standardise.head()
+
+    predict_vector =feature_vector_df
 
     return predict_vector
 
@@ -104,7 +136,9 @@ def make_prediction(data, model):
     """
     # Data preprocessing.
     prep_data = _preprocess_data(data)
+    print("here-----"  ,  prep_data )
     # Perform prediction with model and preprocessed data.
     prediction = model.predict(prep_data)
     # Format as list for output standardisation.
+    print("here-----"  , prediction[0].tolist())
     return prediction[0].tolist()
