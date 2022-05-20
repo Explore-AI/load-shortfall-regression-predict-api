75fd693226dd2da4f4cfa99feea557c12f663b50
moolivieJHB <moolivie@student.wethinkcode.co.za>
2022-05-20 16:48:00 +0200

    1st draft api working . Updates to be made soon.

--- model.py
+++ model.py
@@ -48,7 +48,7 @@ def _preprocess_data(data):
     Pandas DataFrame : <class 'pandas.core.frame.DataFrame'>
         The preprocessed data, ready to be used our model for prediction.
     """
-    
+    print ("here--------" , data)
 
     # Convert the json string to a python dictionary object
     feature_vector_dict = json.loads(data)
@@ -64,35 +64,99 @@ def _preprocess_data(data):
     # ---------------------------------------------------------------
 
     # ----------- Replace this code with your own preprocessing steps --------
-    # predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    
     # ------------------------------------------------------------------------
 
-
-    feature_vector_df ['time'] = pd.to_datetime(feature_vector_df['time'])
-
-    dum_cols = ['Valencia_wind_deg', 'Seville_pressure']
-    df_dummies = pd.get_dummies(feature_vector_df, prefix='dummies', prefix_sep='_', columns=dum_cols, dtype=int, drop_first=True)
-
-    df_dummies.columns = [col.replace(" ","_") for col in df_dummies.columns]
+    #replace with means
+    feature_vector_df['Valencia_pressure'] = feature_vector_df['Valencia_pressure'].fillna(feature_vector_df['Valencia_pressure'].mean())
 
 
+    # both Valencia_wind_deg and Seville_pressure dropped
+    feature_vector_df.drop('Valencia_wind_deg', axis= 1, inplace= True)
+    feature_vector_df.drop('Seville_pressure', axis= 1, inplace= True)
 
-    dum_title = [i for i in df_dummies.columns if i != 'load_shortfall_3h'] + ['load_shortfall_3h']
-    df_dummies = df_dummies.reindex(columns=dum_title)
 
-    feature_vector_df  = df_dummies.copy()
+    # create new features
+    feature_vector_df['time'] = pd.to_datetime(feature_vector_df['time'], format = '%Y-%m-%d %H:%M:%S')
 
-    X = feature_vector_df.drop(['load_shortfall_3h', 'time'], axis=1)
-    y = feature_vector_df['load_shortfall_3h']
+    feature_vector_df['month'] = feature_vector_df['time'].dt.month   # power varies per month depending on season
+    feature_vector_df['day'] = feature_vector_df['time'].dt.day       # power varies depending on day of the week
+    feature_vector_df['hour'] = feature_vector_df['time'].dt.hour 
 
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
 
-    X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
-    X_standardise.head()
+    # feature_vector_df[['month', 'day', 'hour']] = feature_vector_df[['month', 'day', 'hour']].astype('category')
 
-    predict_vector =feature_vector_df
+    # rain_1h_columns = ['Bilbao_rain_1h', 'Barcelona_rain_1h', 'Seville_rain_1h', 'Madrid_rain_1h']
+    # Percentage unique values in all rain_1h columns
+    # As we can see the unique percentage is less than 1%.
+    # Thus only less than 1% of the values is unique.
 
+    # Drop these columns and test models to see if there is a difference.
+
+    # Model performance did increase slightly, thus drop columns.
+
+    # (feature_vector_df[rain_1h_columns].nunique()/ 8763) *100
+
+
+    # weather_id_columns = ['Madrid_weather_id','Barcelona_weather_id','Seville_weather_id', 'Bilbao_weather_id']
+
+    # Percentage unique values in all weather_id columns
+    # As we can see the unique percentage is 4% or less.
+    # Thus only 4% or less of the values is unique.
+
+    # Drop these columns and test models to see if there is a difference.
+
+    # Model performance did decrease slightly, Thus weather_id_columns will not be dropped.
+
+    # (feature_vector_df[weather_id_columns].nunique()/ 8763) *100
+
+    #data 2 model 2
+    # feature_vector_df1= feature_vector_df.copy()
+    # feature_vector_df1 = feature_vector_df.drop(rain_1h_columns, axis=1) 
+
+    predict_vector = feature_vector_df[['month', 'day', 'hour']] = feature_vector_df[['month', 'day', 'hour']].astype('category')
+    # predict_vector = feature_vector_df[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+    # split data
+
+    # y1 = feature_vector_df['load_shortfall_3h']
+    # X1 = feature_vector_df.drop('load_shortfall_3h', axis= 1)
+
+
+
+    # X_train, X_test, y_train, y_test = train_test_split(X1, y1, test_size=0.3332, random_state= PARAMETER_CONSTANT)
+
+
+    # feature_vector_df.drop('Valencia_wind_deg', axis= 1, inplace= True)
+    # feature_vector_df.drop('Seville_pressure', axis= 1, inplace= True)
+
+
+
+
+    # feature_vector_df ['time'] = pd.to_datetime(feature_vector_df['time'])
+
+    # dum_cols = ['Valencia_wind_deg', 'Seville_pressure']
+    # df_dummies = pd.get_dummies(feature_vector_df, prefix='dummies', prefix_sep='_', columns=dum_cols, dtype=int, drop_first=True)
+
+    # df_dummies.columns = [col.replace(" ","_") for col in df_dummies.columns]
+
+
+
+    # dum_title = [i for i in df_dummies.columns if i != 'load_shortfall_3h'] + ['load_shortfall_3h']
+    # df_dummies = df_dummies.reindex(columns=dum_title)
+
+    # feature_vector_df  = df_dummies.copy()
+
+    # X = feature_vector_df.drop(['load_shortfall_3h', 'time'], axis=1)
+    # y = feature_vector_df['load_shortfall_3h']
+
+    # scaler = StandardScaler()
+    # X_scaled = scaler.fit_transform(X)
+
+    # X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
+    # X_standardise.head()
+
+    # predict_vector =feature_vector_df
+    print("***********************************get here done" , predict_vector )
     return predict_vector
 
 def load_model(path_to_model:str):
@@ -136,9 +200,10 @@ def make_prediction(data, model):
     """
     # Data preprocessing.
     prep_data = _preprocess_data(data)
-    print("here-----"  ,  prep_data )
+    print("here-----1")
     # Perform prediction with model and preprocessed data.
+    
     prediction = model.predict(prep_data)
     # Format as list for output standardisation.
-    print("here-----"  , prediction[0].tolist())
+    print("here---the end--"  , prediction[0].tolist())
     return prediction[0].tolist()
--- utils/train_model.py
+++ utils/train_model.py
@@ -13,19 +13,34 @@
 import pandas as pd
 import pickle
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+
+PARAMETER_CONSTANT = 13
 
 # Fetch training data and preprocess for modeling
+print("pickle me this")
 train = pd.read_csv('./data/df_train.csv')
 
-y_train = train[['load_shortfall_3h']]
-X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+# y_train = train[['load_shortfall_3h']]
+# X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
 
+print("pickle me this")
+y2 = train['load_shortfall_3h']
+X2 = train('load_shortfall_3h', axis= 1)
+
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, test_size=0.3332, random_state= PARAMETER_CONSTANT)
 # Fit model
-lm_regression = LinearRegression(normalize=True)
+r_forest_model_1 = RandomForestRegressor(n_estimators = 100, random_state = PARAMETER_CONSTANT)
+# lm_regression = LinearRegression(normalize=True)
 print ("Training Model...")
-lm_regression.fit(X_train, y_train)
+r_forest_model_1.fit(X2,y2)
+r_forest_model_1_pred = r_forest_model_1.predict(X2)
+# lm_regression.fit(y2, X2)
 
 # Pickle model for use within our API
 save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(lm_regression, open(save_path,'wb'))
+pickle.dump(r_forest_model_1_pred   , open(save_path,'wb'))
+# pickle.dump(lm_regression, open(save_path,'wb'))
