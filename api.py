c46b1a90d3a439f1536333f817e8609194807774
Blaqadonis <chinonsoodiaka@gmail.com>
2022-05-26 18:25:19 +0100

    Update api.py

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 # Load our model into memory.
 # Please update this path to reflect your own trained model.
 static_model = load_model(
-    path_to_model='assets/trained-models/load_shortfall_simple_lm_regression.pkl')
+    path_to_model='assets/trained-models/RF.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
