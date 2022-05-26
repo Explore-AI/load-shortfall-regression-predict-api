d255db4b7eb38c9c01ced852b203349634b538c6
moolivieJHB <moolivie@student.wethinkcode.co.za>
2022-05-26 08:10:18 +0200

    Group3 Random Forest Model

--- utils/train_model.py
+++ utils/train_model.py
@@ -16,6 +16,9 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import StandardScaler
+
+import xgboost as xgb
 
 PARAMETER_CONSTANT = 13
 
@@ -23,24 +26,69 @@ PARAMETER_CONSTANT = 13
 print("pickle me this")
 train = pd.read_csv('./data/df_train.csv')
 
-# y_train = train[['load_shortfall_3h']]
-# X_train = train[['Madrid_wind_speed','Bilbao_rain_1h','Valencia_wind_speed']]
+train['time'] = pd.to_datetime(train['time'])
 
-print("pickle me this")
+train['time'] = pd.to_datetime(train['time'], format = '%Y-%m-%d %H:%M:%S')
+
+train['year'] = train['time'].dt.year     # year value is arbitrary where power is concerned
+train['month'] = train['time'].dt.month   # power varies per month depending on season
+train['day'] =train['time'].dt.day       # power varies depending on day of the week
+train['hour'] =train['time'].dt.hour     # power varies depending on the time of the day
+
+train[['month', 'day', 'hour']] = train[['month', 'day', 'hour']].astype('int64')
+
+df_date = [i for i in train.columns if i != 'load_shortfall_3h'] + ['load_shortfall_3h']
+train= train.reindex(columns=df_date)
+
+    #Extracting the numeric on our data but datatype still object
+train['Seville_pressure'] = train['Seville_pressure'].str.extract('(\d+)')
+train['Valencia_wind_deg'] = train['Valencia_wind_deg'].str.extract('(\d+)')
+
+    #converting object into numeric data type
+train['Seville_pressure'] = pd.to_numeric(train['Seville_pressure'])
+train['Valencia_wind_deg'] = pd.to_numeric(train['Valencia_wind_deg'])
+
+X = train.drop(['load_shortfall_3h', 'time'], axis=1)
+y = train['load_shortfall_3h']
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
+X_standardise = pd.DataFrame(X_scaled, columns=X.columns)
+
+
+
+train = train.drop(['Seville_pressure', 'Valencia_wind_deg', 'time' , 'Valencia_pressure'], axis=1)
+
+X2 = train.drop('load_shortfall_3h', axis= 1)
 y2 = train['load_shortfall_3h']
-X2 = train('load_shortfall_3h', axis= 1)
 
-X_train2, X_test2, y_train2, y_test2 = train_test_split(X2, y2, test_size=0.3332, random_state= PARAMETER_CONSTANT)
 # Fit model
-r_forest_model_1 = RandomForestRegressor(n_estimators = 100, random_state = PARAMETER_CONSTANT)
+rfr = RandomForestRegressor(n_estimators =200, max_depth=None, max_features='auto', bootstrap=True, random_state =PARAMETER_CONSTANT)
+trees = DecisionTreeRegressor(max_depth=15, random_state = PARAMETER_CONSTANT)
+
+# r_forest_model_1 = RandomForestRegressor(n_estimators = 100, random_state = PARAMETER_CONSTANT)
+# xgb_model = xgb.XGBRegressor(base_score=0.7, booster='gbtree', n_estimators=12, learning_rate=0.333, gamma=15, max_depth=5, eval_metric='rmse', random_state=42, reg_alpha=15, reg_lambda=10)
 # lm_regression = LinearRegression(normalize=True)
 print ("Training Model...")
-r_forest_model_1.fit(X2,y2)
-r_forest_model_1_pred = r_forest_model_1.predict(X2)
+# trees.fit(X2 ,y2)
+rfr.fit(X2,y2)
+# rfr_pred_train1 = rfr.predict(X2)
+
+# xgb_model.fit(X2,y2)
+# xgb_pred_train1 = xgb_model.predict(X2)
+
+# r_forest_model_1.fit(X2,y2)
+# r_forest_model_1_pred = r_forest_model_1.predict(X2)
 # lm_regression.fit(y2, X2)
 
 # Pickle model for use within our API
-save_path = '../assets/trained-models/load_shortfall_simple_lm_regression.pkl'
+save_path = '../assets/trained-models/load_shortfall_rfr_regression.pkl'
 print (f"Training completed. Saving model to: {save_path}")
-pickle.dump(r_forest_model_1_pred   , open(save_path,'wb'))
+
+# pickle.dump(xgb , open(save_path,'wb'))
+pickle.dump(rfr , open(save_path,'wb'))
 # pickle.dump(lm_regression, open(save_path,'wb'))
