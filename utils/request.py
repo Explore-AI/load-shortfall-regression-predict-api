c8d6bc87831b7b6e5704f1d663361dc9c3001a21
Roger Arendse <roger.arendse713@gmail.com>
2022-05-26 14:24:49 +0200

    API - Update

--- utils/request.py
+++ utils/request.py
@@ -21,14 +21,11 @@
 import requests
 import pandas as pd
 import numpy as np
-import os
 
 # Load data from fillse to send as an API POST request.
 # We prepare a DataFrame with the public test set + riders data
 # from the Kaggle challenge.
-#path = r'C:\Users\Roger Arendse\Desktop\EDSA\Technical\5. Advanced Regression\1. Predict\Final_API\load-shortfall-regression-predict-api\utils\data\df_train.csv'
-#relative_path = os.path.relpath(path)
-test = pd.read_csv('../data/df_train.csv')
+test = pd.read_csv("utils/data/df_train.csv")
 
 
 # Convert our DataFrame to a JSON string.
