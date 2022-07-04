765c3a924ba2ab7bbd39789a42c0f1ea6dff6db9
Calvin Mngomezulu <91868701+vellyMngo@users.noreply.github.com>
2022-07-04 16:41:24 +0200

    Update api.py

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ app = Flask(__name__)
 # Load our model into memory.
 # Please update this path to reflect your own trained model.
 static_model = load_model(
-    path_to_model='assets/trained-models/XGBRegressor.pkl')
+    path_to_model='assets/trained-models/rg3_model.pkl')
 
 print ('-'*40)
 print ('Model successfully loaded')
