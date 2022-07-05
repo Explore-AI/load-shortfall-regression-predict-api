d4613e090dfc4a1d2b8ed0d7a655ff9a48f56cf7
Calvin Mngomezulu <91868701+vellyMngo@users.noreply.github.com>
2022-07-05 22:10:32 +0200

    Update request.py

--- utils/request.py
+++ utils/request.py
@@ -37,7 +37,7 @@ feature_vector_json = test.iloc[1].to_json()
 # replace the URL below with its public IP:
 
 # url = 'http://{public-ip-address-of-remote-machine}:5000/api_v0.1'
-url = 'http://176.34.130.91:5000/api_v0.1'
+url = 'http://3.249.158.5:5000/api_v0.1'
 
 # Perform the POST request.
 print(f"Sending POST request to web server API at: {url}")
